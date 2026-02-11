a32dec5764912de0547b92a49fa9ac32ba1814b2
jarodCode <quentin.jacquot10@gmail.com>
2026-02-11 08:55:20 +0100

    initial commit

---
+++ quick_test.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+response = llm.invoke("Hello! Test message.")
+print(response.content)
